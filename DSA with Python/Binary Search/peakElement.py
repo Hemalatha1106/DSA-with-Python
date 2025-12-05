e248dfb4c2b02d92cbe827e469252b9fdc3cde8d
Hemalatha1106 <hemavenkat1124@gmail.com>
2025-12-05 12:04:57 +0530

    mkdir binary search

---
+++ DSA with Python/Binary Search/peakElement.py
@@ -0,0 +1,19 @@
+n = len(nums)
+if n == 1:
+    return 0
+if nums[0] > nums[1]:
+    return 0
+if nums[n-1] > nums[n-2]:
+    return n-1        
+l = 1
+r = n-1
+while l <= r:
+    mid = (l+r)//2
+    if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+        return mid
+    if nums[mid] > nums[mid-1]:
+        l = mid + 1
+    else:
+        r = mid-1
+return -1                    
+#https://leetcode.com/problems/find-peak-element
