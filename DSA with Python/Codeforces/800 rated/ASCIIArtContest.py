9b056b29d1ec954b883016749b362eb528d1eaaf
Hemalatha1106 <hemavenkat1124@gmail.com>
2025-12-06 14:08:15 +0530

    restored

--- DSA with Python/Codeforces/800 rated/ASCIIArtContest.py
+++
@@ -1,6 +0,0 @@
-nums = list(map(int,input().split()))
-nums.sort()
-if nums[-1]-nums[0] >= 10:
-    print("check again")
-else:
-    print("final",nums[1])    
--- DSA with Python/Codeforces/800 rated/YourName.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    num = int(input())
-    s1, s2 = input().split()
-    if sorted(s1) == sorted(s2):
-        print("YES")
-    else:
-        print("No")
-            
