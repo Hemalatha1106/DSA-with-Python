c9e6d4d5abef3d336061d56778536ef97fada454
Hemalatha1106 <hemavenkat1124@gmail.com>
2025-12-08 02:05:50 +0530

    implented

--- GRAPHS/biparatiteGraph.py
+++ GRAPHS/biparatiteGraph.py
@@ -10,3 +10,20 @@ graph = {
     8: [7]
 }
 n = len(graph)
+visited = [-1 for i in range(n+1)]
+def dfs(graph, c, visited, node):
+    visited[node] = c
+    for i in graph[node]:
+        if visited[i] != -1:
+            if visited[i] == c:
+                return False
+        else:
+            if not dfs(graph, 1-c, visited, i):
+                return False
+    return True
+for j in graph:
+    if visited[j] == -1:
+        if not dfs(graph, 0, visited, j):
+            print("Not a Biparatite graph")
+            exit()
+print("Biparatite graph")               
