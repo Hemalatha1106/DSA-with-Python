71b644a1862cf4e7b91e388f5dec435de0de0944
Hemalatha1106 <hemavenkat1124@gmail.com>
2025-12-10 08:02:04 +0530

    done with course schedule 1 lc

---
+++ GRAPHS/courseSchedule1.py
@@ -0,0 +1,23 @@
+numCourses = 2
+prerequisites = [[1,0],[0,1]]
+q = []
+topo = []
+adj = [[] for i in range(numCourses)]
+indegree = [0]*numCourses
+for u, v in prerequisites:
+    adj[u].append(v)
+    indegree[v] += 1
+for j in range(len(adj)):
+    if indegree[j] == 0:
+        q.append(j)
+while q:
+    u = q.pop(0)
+    topo.append(u)
+    for v in adj[u]:
+        indegree[v] -= 1
+        if indegree[v] == 0:
+            q.append(v)
+if len(topo) != numCourses:
+    print(False)
+else:
+    print(True)                          
